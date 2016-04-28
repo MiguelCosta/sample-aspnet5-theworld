1a02209396408b90ebf5f5915d50a95ec72c683b
Miguel Costa <miguelpintodacosta@gmail.com>
2016-04-29 00:19:01 +0100

    Add angular

---
+++ TheWorldVs2015/src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app-trips')
+        .controller('tripsController', tripsController);
+
+    //tripsController.$inject = ['$location'];
+
+    function tripsController($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.name = 'Miguel Costa';
+        vm.trips = [
+            {
+                name: "US Trip",
+                created: new Date()
+            },
+            {
+                name: "World Trip",
+                created: new Date()
+            }
+
+        ];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function () {
+            alert(vm.newTrip.name);
+        };
+
+        //activate();
+
+        //function activate() { }
+    }
+})();
