f5553a8acc5e923bdcbc49b12bd87df3d2b5954c
Miguel Costa <miguelpintodacosta@gmail.com>
2016-04-30 13:38:20 +0100

    Minify javascript

--- TheWorldVs2015/src/TheWorld/wwwroot/js/tripEditorController.js
+++ TheWorldVs2015/src/TheWorld/wwwroot/js/tripEditorController.js
@@ -3,7 +3,7 @@
     "use strict";
 
     angular.module("app-trips")
-        .controller("tripEditorController", tripEditorController);
+      .controller("tripEditorController", tripEditorController);
 
     function tripEditorController($routeParams, $http) {
         var vm = this;
@@ -17,53 +17,53 @@
         var url = "/api/trips/" + vm.tripName + "/stops";
 
         $http.get(url)
-            .then(function (response) {
-                console.log(JSON.stringify(response));
-                angular.copy(response.data, vm.stops);
-                _showMap(vm.stops);
-            }, function (error) {
-                vm.errorMessage = "Failed load stops: " + error;
-            }).finally(function () {
-                vm.isBusy = false;
-            });
+          .then(function (response) {
+              // success
+              angular.copy(response.data, vm.stops);
+              _showMap(vm.stops);
+          }, function (err) {
+              // failure
+              vm.errorMessage = "Failed to load stops";
+          })
+          .finally(function () {
+              vm.isBusy = false;
+          });
 
         vm.addStop = function () {
             vm.isBusy = true;
-            vm.errorMessage = "";
 
             $http.post(url, vm.newStop)
               .then(function (response) {
                   // success
                   vm.stops.push(response.data);
-                  vm.newStop = {};
                   _showMap(vm.stops);
+                  vm.newStop = {};
               }, function (err) {
                   // failure
-                  vm.errorMessage = "Failed to save new stop: " + err;
+                  vm.errorMessage = "Failed to add new stop";
               })
               .finally(function () {
                   vm.isBusy = false;
               });
-        }
+        };
     }
 
     function _showMap(stops) {
         if (stops && stops.length > 0) {
             var mapStops = _.map(stops, function (item) {
-                var ms = {
+                return {
                     lat: item.latitude,
                     long: item.longitude,
                     info: item.name
                 };
-                return ms;
             });
 
-            console.log(JSON.stringify(mapStops));
-            // show map
+            // Show Map
             travelMap.createMap({
                 stops: mapStops,
-                selector: "#map", // local inde the page
-                initialZoom: 5
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
             });
         }
     }
